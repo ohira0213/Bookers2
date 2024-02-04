3b8a26b540f1fad702384114ad36cfb7aa05f02c
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-02-04 17:58:49 +0000

    Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'home/about' => 'homes#about', as: 'about'
+  get '/about' => 'homes#about', as: 'about'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy, :new]
   resources :users, only: [:index, :show, :edit, :update]
   #resources :user_registrations
