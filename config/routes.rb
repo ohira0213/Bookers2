f2c2cd59521b17f99f7ada1a3ea94bd07ffd04e0
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-03-07 06:50:36 +0000

    Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/about' => 'homes#about', as: 'about'
-<<<<<<< HEAD
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy, :new]
-=======
-  
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy, :new] do
-    resource :favorite, only: [:create, :destroy]
-    resources :book_comments, only: [:create, :destroy]
-  end
->>>>>>> origin/main
   resources :users, only: [:index, :show, :edit, :update]
 end
